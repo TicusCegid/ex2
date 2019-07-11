1b5f12fb8a1247e5cbeb56d32c1876fffbbc1598
Malo <malo.soreau@gmail.com>
2019-07-11 11:56:16 +0200

    Creation du fichier blackJack.cpp et remodification des regles

---
+++ BlackJack.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+//Ceci est le fichier du projet de blackJack
+
+
+using namespace std;
+
+
+
+int main(int argc, char const *argv[]) {
+  cout << "Hello World!" << endl;
+  cout << "Nouvelle Partie de BlackJack" << endl;
+  return 0;
+}
---
+++ Regle.md
@@ -0,0 +1,5 @@
+Ce fichier contient les règles du blackJack
+
+Valeur des cartes :
+La valeur d'une carte varie entre 2 et 10 elle est indiquée sur la carte. Une figure vaut 10 points.
+L'As vaut 1 ou 11 selon la valeur que lui donne le joueur.
